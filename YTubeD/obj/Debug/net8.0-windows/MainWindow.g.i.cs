e0405afe6c2e42ef757073f1069933e372d243d5
vancoder1 <ivanzaporozhets25@gmail.com>
2024-02-14 22:11:29 -0700

    Add ObservableObject; Change structure of the project a little; Add styles

--- YTubeD/obj/Debug/net8.0-windows/MainWindow.g.i.cs
+++ YTubeD/obj/Debug/net8.0-windows/MainWindow.g.i.cs
@@ -53,7 +53,7 @@ namespace YTubeD {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/YTubeD;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/YTubeD;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);

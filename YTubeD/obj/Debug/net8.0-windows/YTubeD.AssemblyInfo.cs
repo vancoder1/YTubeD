14320123a4ccca7141508161a36db1eb9a81aa57
vancoder1 <ivanzaporozhets25@gmail.com>
2024-02-14 22:21:21 -0700

    master commit

--- YTubeD/obj/Debug/net8.0-windows/YTubeD.AssemblyInfo.cs
+++ YTubeD/obj/Debug/net8.0-windows/YTubeD.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YTubeD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba9dfa3751e13381ff7f06f4a7603f100e1f03fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("YTubeD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YTubeD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

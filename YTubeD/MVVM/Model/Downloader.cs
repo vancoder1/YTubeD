1c9eda6dbbc9d8aaa5d53f2aa8044bacff2fbb5f
vancoder1 <ivanzaporozhets25@gmail.com>
2024-03-06 23:37:15 -0700

    Add Downloader and YoutubeVideo models that will be modified in next commits

---
+++ YTubeD/MVVM/Model/Downloader.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YTubeD.Core;
+
+namespace YTubeD.MVVM.Model
+{
+    internal class Downloader : ObservableObject
+    {
+        public YoutubeVideo Video { get; set; }
+        public string OutputDirectory { get; set; }
+
+        public Downloader()
+        {
+            Video = new YoutubeVideo();
+            OutputDirectory = "";
+        }
+    }
+}
---
+++ YTubeD/MVVM/Model/YoutubeVideo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YTubeD.Core;
+
+namespace YTubeD.MVVM.Model
+{
+    internal class YoutubeVideo : ObservableObject
+    {
+        public string Url { get; set; }
+        public string VideoName { get; set; }
+        
+        public YoutubeVideo()
+        {
+            Url = "";
+            VideoName = "";
+        }
+    }
+}

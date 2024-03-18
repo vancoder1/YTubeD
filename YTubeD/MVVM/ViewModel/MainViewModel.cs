22ed46c0455dda8fec71c42d913f46c9a8279a26
vancoder1 <ivanzaporozhets25@gmail.com>
2024-03-18 12:33:30 -0600

    Bind IsUrlValid to video quality ComboBox

--- YTubeD/MVVM/ViewModel/MainViewModel.cs
+++ YTubeD/MVVM/ViewModel/MainViewModel.cs
@@ -4,6 +4,7 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using YTubeD.Core;
 using YTubeD.MVVM.Model;
 
@@ -12,6 +13,19 @@ namespace YTubeD.MVVM.ViewModel
     class MainViewModel : ObservableObject
     {
         Downloader YTDownloader { get; set; }
+        private bool _isUrlValid;
+        public bool IsUrlValid
+        {
+            get { return _isUrlValid; }
+            set
+            {
+                if (_isUrlValid != value)
+                {
+                    _isUrlValid = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
         public string Url
         {
             get { return YTDownloader.Video.Url; }
@@ -19,6 +33,7 @@ namespace YTubeD.MVVM.ViewModel
             {
                 YTDownloader.Video.Url = value;
                 OnPropertyChanged();
+                ValidateUrlCommand.Execute(null);
             }
         }
         public string SavingPath
@@ -30,10 +45,20 @@ namespace YTubeD.MVVM.ViewModel
                 OnPropertyChanged();
             }
         }
+        public ICommand ValidateUrlCommand { get; }
 
         public MainViewModel()
         {
             YTDownloader = new Downloader();
+            ValidateUrlCommand = new RelayCommand(ValidateUrl);
+        }
+        private async void ValidateUrl(object parameter)
+        {
+            IsUrlValid = await IsYoutubeUrlValid();
+        }
+        private async Task<bool> IsYoutubeUrlValid()
+        {
+            return await YTDownloader.IsUrlValid();
         }
     }
 }

b8d8065b87f539ca6b5af73087c65ff85841835c
vancoder1 <ivanzaporozhets25@gmail.com>
2024-03-22 12:27:34 -0600

    Change the gitignore structure using dotnet new gitignore

--- YTubeD/obj/Debug/net8.0-windows/YTubeD.AssemblyInfo.cs
+++ YTubeD/obj/Debug/net8.0-windows/YTubeD.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YTubeD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+977d8343d3277b7ab36fa4c939f0558e092003f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3e5f34bfb356ca2ef1f807c936abc9ef8475bc4")]
 [assembly: System.Reflection.AssemblyProductAttribute("YTubeD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YTubeD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

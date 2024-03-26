2fbf0f10a619d1e9cf3f0365fc289a2db2943ce2
vancoder1 <ivanzaporozhets25@gmail.com>
2024-03-25 22:10:56 -0600

    Add icon and title to the project's top bar

--- YTubeD/obj/Debug/net8.0-windows/YTubeD.AssemblyInfo.cs
+++ YTubeD/obj/Debug/net8.0-windows/YTubeD.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YTubeD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8d8065b87f539ca6b5af73087c65ff85841835c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+714427661fc49b6acd24054a8fb7f54263ffc1b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("YTubeD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YTubeD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

a180ec4f019d108835baaf40f81a6b1d87c33939
vancoder1 <ivanzaporozhets25@gmail.com>
2024-03-26 21:14:36 -0600

    Committing local changes before merging with develop

--- YTubeD/obj/Debug/net8.0-windows/YTubeD.AssemblyInfo.cs
+++ YTubeD/obj/Debug/net8.0-windows/YTubeD.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YTubeD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+250b01e5876c34b415f1412f59362daa8117c0ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b6f22ccfea95908344f3924087b761111377bb3")]
 [assembly: System.Reflection.AssemblyProductAttribute("YTubeD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YTubeD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

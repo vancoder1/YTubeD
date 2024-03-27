577b62b57d941a2a6ec4125d8b3d5e45bce54f12
vancoder1 <ivanzaporozhets25@gmail.com>
2024-03-27 01:04:34 -0600

    Start changing layout of the whole application; Add topbar

--- YTubeD/obj/Debug/net8.0-windows/YTubeD.AssemblyInfo.cs
+++ YTubeD/obj/Debug/net8.0-windows/YTubeD.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YTubeD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fbf0f10a619d1e9cf3f0365fc289a2db2943ce2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a25b469339ab83bdb5d2092b26c13a3b4aa1680")]
 [assembly: System.Reflection.AssemblyProductAttribute("YTubeD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YTubeD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

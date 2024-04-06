dbbc870c397c2a325c7971bac80e784da9c3a253
Ivan Zaporozhets <ivanzaporozhets25@gmail.com>
2024-04-06 00:30:33 -0600

    Rebuild both debug and release

--- YTubeD/obj/Debug/net8.0-windows/YTubeD.AssemblyInfo.cs
+++ YTubeD/obj/Debug/net8.0-windows/YTubeD.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YTubeD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d10a74a8a6c638ef80de8c9752779f3a387ced2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+533ecacab880149cab77c767d02c4939e5965205")]
 [assembly: System.Reflection.AssemblyProductAttribute("YTubeD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YTubeD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
